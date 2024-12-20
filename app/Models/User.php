a64e401c53d176e6ea079e7583d20d6945dd9bac
Kunti Najma Jalia <najmajalia02@gmail.com>
2024-12-20 23:13:06 +0700

    model roles & many-to-many relation to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -49,5 +50,11 @@ class User extends Authenticatable
     public function chirps(): HasMany
     {
         return $this->hasMany(Chirp::class);
+    }
+
+    // kode untuk relasi ke model Role (many-to-many)
+    public function roles() : BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
     }
 }
